391d08d721d8d5b1c40bde597c995f4b5a656213
yzjc <yzjc18511562884@outlook.com>
2026-04-25 23:45:56 +0800

    init spring boot backend

---
+++ backend/src/test/java/com/jiacheng/securevault/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiacheng.securevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
